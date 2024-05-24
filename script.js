28d5e61010012cbfed031a5d5756c933d03cf666
VighneshG <vighneshkumariit@gmail.com>
2024-05-25 00:04:48 +0530

    updated scripts

--- script.js
+++ script.js
@@ -18,49 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 
-  document.getElementById('downloadBtn').addEventListener('click', () => {
+  document.getElementById("downloadBtn").addEventListener("click", () => {
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
-    // Define the dimensions of the A4 page
-    const pageWidth = 210;
-    const pageHeight = 297;
-
     // Adjust the margins and scale the content to fit within the PDF
-    const content = document.querySelector('.cv-container');
+    const content = document.querySelector(".cv-container");
     doc.html(content, {
-        callback: function (doc) {
-            doc.save('resume.pdf');
-        },
-        x: 0, // Position at top-left corner
-        y: 0,
-        width: pageWidth, // Set width to A4 width
-        height: pageHeight // Set height to A4 height
+      callback: function (doc) {
+        doc.save("resume.pdf");
+      },
+      x: 10,
+      y: 10,
+      html2canvas: { scale: 0.25 },
     });
-});
-
-  // document.getElementById('downloadBtn').addEventListener('click', () => {
-  //     const { jsPDF } = window.jspdf;
-  //     const doc = new jsPDF();
-
-  //     // Define the dimensions of the A4 page
-  //     const pageWidth = 210;
-  //     const pageHeight = 297;
-
-  //     // Adjust the margins and scale the content to fit within the PDF
-  //     const content = document.querySelector('.cv-container');
-  //     doc.html(content, {
-  //         callback: function (doc) {
-  //             doc.save('resume.pdf');
-  //         },
-  //         x: 0,
-  //         y: 0,
-  //         html2canvas: {
-  //             scale: 1, // Set the scale to 1 to capture full resolution
-  //             width: pageWidth, // Set width to A4 width
-  //             height: pageHeight // Set height to A4 height
-  //         },
-  //         margin: { top: 20, right: 20, bottom: 20, left: 20 }, // Adjust margins as needed
-  //     });
-  // });
+  });
 });
